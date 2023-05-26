1834e6906d1efda1d6df22540407626600068e3e
engahmedalhopi <ahmedalhopi991@gmail.com>
2023-05-26 17:36:52 +0300

    26-5-2023 5:36 PM

--- JavaApplication/src/javaapplication/PresenceAndAbsenceController.java
+++ JavaApplication/src/javaapplication/PresenceAndAbsenceController.java
@@ -10,6 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -31,6 +32,8 @@ public class PresenceAndAbsenceController implements Initializable {
     private RadioButton absent_status;
     @FXML
     private ToggleGroup radioSelect;
+    @FXML
+    private Button btnGet;
 
     ObservableList<ObservableList<String>> dataTable = FXCollections.observableArrayList();
     ObservableList<String> row;
@@ -62,7 +65,7 @@ public class PresenceAndAbsenceController implements Initializable {
             System.out.println(ex);
         }
 
-        sel = "SELECT  lecture_id, student_number, student_name, student_mobile,'0' as status FROM managment.attendance where lecture_id = ? ;";
+        sel = "SELECT  lecture_id, student_number, student_name, student_mobile, status FROM managment.attendance where lecture_id = ? order by student_number;";
         try {
             conn = DatabaseConnect.connDB();
             pst = conn.prepareStatement(sel);
@@ -71,14 +74,14 @@ public class PresenceAndAbsenceController implements Initializable {
             rs = pst.executeQuery();
             System.out.println(rs);
             while (rs.next()) {
-                ObservableList<String> row = FXCollections.observableArrayList();
-                row.add(rs.getString("lecture_id"));
-                row.add(rs.getString("student_number"));
-                row.add(rs.getString("student_name"));
-                row.add(rs.getString("student_mobile"));
-                row.add(rs.getString("status"));
-                dataTable.add(row);
-                System.out.println(row);
+                ObservableList<String> record = FXCollections.observableArrayList();
+                record.add(rs.getString("lecture_id"));
+                record.add(rs.getString("student_number"));
+                record.add(rs.getString("student_name"));
+                record.add(rs.getString("student_mobile"));
+                record.add(rs.getString("status"));
+                dataTable.add(record);
+                System.out.println(record);
             }
             row = dataTable.get(0);
             String[] rowData = row.toArray(new String[0]);
@@ -89,6 +92,7 @@ public class PresenceAndAbsenceController implements Initializable {
             present_status.setSelected(false);
             absent_status.setSelected(false);
             lecture_id.setEditable(false);
+            btnGet.setDisable(true);
             System.out.println(dataTable.size());
         } catch (SQLException ex) {
             System.out.println(ex);
@@ -123,26 +127,24 @@ public class PresenceAndAbsenceController implements Initializable {
                 int no_lect = Integer.parseInt(lecture_id.getText());
                 pst.setInt(3, no_lect);
                 pst.executeUpdate();
+                row.set(4, status_option);
 
                 ++i;
                 row = dataTable.get(i);
-                String[] rowData = row.toArray(new String[0]);
+                String[] rowData = row.toArray(new String[i]);
                 student_number.setText(rowData[1]);
                 student_name.setText(rowData[2]);
                 student_mobile.setText(rowData[3]);
-                row.set(4, status_option);
-                System.out.println(row);
-                System.out.println(dataTable);
-                if (!"0".equals(row.get(4))) {
-                    if ("Present".equals(row.get(4))) {
-                        present_status.setSelected(true);
-                    } else {
-                        absent_status.setSelected(true);
-                    }
+                if ("Present".equals(row.get(4))) {
+                    present_status.setSelected(true);
+                } else if ("Absent".equals(row.get(4))) {
+                    absent_status.setSelected(true);
                 } else {
                     present_status.setSelected(false);
                     absent_status.setSelected(false);
                 }
+                System.out.println(row.get(4));
+                System.out.println(dataTable);
 
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, ex);
@@ -152,64 +154,35 @@ public class PresenceAndAbsenceController implements Initializable {
         } else {
             JOptionPane.showMessageDialog(null, "Finsed all students");
             lecture_id.setEditable(true);
+
         }
     }
 
     public void previousStudent() throws ClassNotFoundException {
         if (i >= 0) {
 
-            String status_option = "";
-
-            PreparedStatement pst;
-            Connection conn;
-            String sel = "UPDATE managment.attendance SET  status = ? WHERE student_number = ? and lecture_id = ?;";
-            try {
-                conn = DatabaseConnect.connDB();
-                pst = conn.prepareStatement(sel);
-
-                RadioButton selectedRadioButton = (RadioButton) radioSelect.getSelectedToggle();
-                if (selectedRadioButton != null) {
-                    if (selectedRadioButton == present_status) {
-                        status_option = "Present";
-                    } else if (selectedRadioButton == absent_status) {
-                        status_option = "Absent";
-                    }
-                }
-
-                pst.setString(1, status_option);
-                pst.setString(2, student_number.getText());
-                int no_lect = Integer.parseInt(lecture_id.getText());
-                pst.setInt(3, no_lect);
-                pst.executeUpdate();
-
                 --i;
                 row = dataTable.get(i);
-                String[] rowData = row.toArray(new String[0]);
+                String[] rowData = row.toArray(new String[i]);
                 student_number.setText(rowData[1]);
                 student_name.setText(rowData[2]);
                 student_mobile.setText(rowData[3]);
-                row.set(4, status_option);
-                System.out.println(row);
-                System.out.println(dataTable);
-                if (!"0".equals(row.get(4))) {
-                    if ("Present".equals(row.get(4))) {
-                        present_status.setSelected(true);
-                    } else {
-                        absent_status.setSelected(true);
-                    }
+                if ("Present".equals(row.get(4))) {
+                    present_status.setSelected(true);
+                } else if ("Absent".equals(row.get(4))) {
+                    absent_status.setSelected(true);
                 } else {
                     present_status.setSelected(false);
                     absent_status.setSelected(false);
                 }
+                System.out.println(row.get(4));
+                System.out.println(dataTable);
 
-            } catch (SQLException ex) {
-                JOptionPane.showMessageDialog(null, ex);
-                System.out.println(ex);
-            }
 
         } else {
             JOptionPane.showMessageDialog(null, "This is first student");
             lecture_id.setEditable(true);
+            btnGet.setDisable(false);
         }
     }
 
